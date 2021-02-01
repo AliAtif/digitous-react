6d59b408388f07364596a8121bc88710a06e4cce
atif <at07.if@gmail.com>
2021-02-01 15:05:04 +0100

    elsy terminé

--- src/App.js
+++ src/App.js
@@ -23,26 +23,57 @@ export class App extends React.Component {
     };
   };
 
+  
+  onCalculateWater() {
+    let water = this.state.water;
+    if (this.state.temperature > 20) {
+      const nbSup20 = this.state.temperature - 20
+      const nbLiterToAdd = 0.02 * nbSup20;
+      water = water + nbLiterToAdd;
+    }
+
+    if (this.state.heart > 120) {
+      const nbHeartBeatSup = this.state.heart - 120;
+      const nbLiterToAdd = 0.0008 * nbHeartBeatSup
+      water = water + nbLiterToAdd
+      console.log('waterHeart', water)
+    }
+
+    if (this.state.steps > 10000) {
+      const nbHeartBeatSup = this.state.steps - 10000;
+      const nbLiterToAdd = 0.00002 * nbHeartBeatSup
+      water = water + nbLiterToAdd
+    }
+    this.setState({
+      water
+    })
+  }
+
   onHeartChange = (value) =>{
     console.log(value.target.value);
     this.setState({
       heart : value.target.value,
-    })
+    });
+    this.onCalculateWater()
    
   };
 
-  onStepsChange= value =>{console.log(value.target.value)
-    this.setState({ steps:value.target.value})
+  onStepsChange= value =>{
+    console.log(value.target.value)
+    this.setState({ 
+      steps:value.target.value
+    });
+    this.onCalculateWater()
   };
 
   onTemperatureChange = (value) =>{
     console.log(value.target.value);
     this.setState({
       temperature : value.target.value,
-    })
+    });
+    this.onCalculateWater()
    
   };
-   
 
   render() {
 
@@ -54,7 +85,7 @@ export class App extends React.Component {
           <p> Temperature :{tempMin}</p>
           <p> Steps:{stepsMin}</p>
 
-        < Box icon ={"local_drink"} color={"#3A85FF"} value={1.5}  unit={"L"}  />
+        < Box icon ={"local_drink"} color={"#3A85FF"} value={this.state.water} unit={"L"}  />
         < Box icon ={"directions_walk"} color= "#000000" value = {this.state.steps} unit = {"steps"} min={stepsMin} max={stepsMax} onChange = {this. onStepsChange} />
         < Box icon = {"favorite"} color = {"#ff0000"}value= {this.state.heart} unit ={"bpm"} min={heartMin} max= {heartMax} onChange = {this.onHeartChange} />
         < Box icon = {"wb_sunny"} color = {"#ffff00"} value = {this.state.temperature} unit ={"°C"} min={tempMin} max= {tempMax} onChange = {this.onTemperatureChange} />
